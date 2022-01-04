1dc6b8b58a77bafa373a74bb99549c47b6b8f1ce
Dmitrii Kryzhanovskii <dmitr.kryzh@gmail.com>
2022-01-04 15:43:09 +0300

    Update 1.py

--- Задание_2/1/1.py
+++ Задание_2/1/1.py
@@ -1,8 +1,8 @@
-print('c','a','d','b')
-for a in 0,1:
+print('c','a','d','b') # head of the table
+for a in 0,1: #possible binary numbers
    for b in 0,1:
       for c in 0,1:
          for d in 0,1:
             if ((a and b) !=(c)) and (b<=d):
-               print(c,a,d,b)
+               print(c, a, d, b)  # outputs already in the correct form
 #ans:cadb
