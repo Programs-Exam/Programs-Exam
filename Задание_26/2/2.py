20c5481de796bd9b4f03797b1f6520eb3677728f
Dmitrii Kryzhanovskii <dmitr.kryzh@gmail.com>
2022-02-05 21:09:51 +0200

    Rename

--- Задание_26/2/2.py
+++
@@ -1,9 +0,0 @@
-f = open('26-k2.txt')
-izm = []
-n, k= map(int, f.readline().split())
-for line in f:
-   izm += [int(line)]
-izm=sorted(izm)
-ned = izm[:k]+izm[-k:]
-izm = izm[k:-k]
-print(izm[0],sum(ned)//len(ned))
